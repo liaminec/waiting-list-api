729183f999f1b6e2f8ef708e400d4c29f2b74fee
liaminec <liaminec@gmail.com>
2025-09-03 00:27:39 +0200

    added sqlite databases + data files + script to load initial data

---
+++ api/data/init_db.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sqlalchemy.exc import IntegrityError, OperationalError
+from sqlmodel import Session, text
+
+from config import engine
+
+
+def load_data(file_path: str, table: str) -> None:
+    data = pd.read_csv(file_path, index_col="id")
+    data.to_sql(table, engine, if_exists="append")
+
+
+if __name__ == '__main__':
+    print("Loading data into the DB")
+    try:
+        load_data("api/data/files/users.csv", "user")
+        load_data("api/data/files/organizations.csv", "organization")
+        load_data("api/data/files/events.csv", "event")
+        load_data("api/data/files/representations.csv", "representation")
+        load_data("api/data/files/offer_types.csv", "offertype")
+        load_data("api/data/files/offers.csv", "offer")
+        load_data("api/data/files/inventory.csv", "inventory")
+    except (IntegrityError, OperationalError):
+        with Session(engine) as session:
+            session.execute(text("DELETE FROM inventory"))
+            session.execute(text("DELETE FROM offer"))
+            session.execute(text("DELETE FROM offer_type"))
+            session.execute(text("DELETE FROM representation"))
+            session.execute(text("DELETE FROM event"))
+            session.execute(text("DELETE FROM organization"))
+            session.execute(text("DELETE FROM user"))
+    print("The data was successfully loaded")
