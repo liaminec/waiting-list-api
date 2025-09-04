1f52a9743312c734864a502e21c83df72545f944
liaminec <liaminec@gmail.com>
2025-09-04 02:12:06 +0200

    added serializers for models of events subapp

--- api/events/serializers.py
+++ api/events/serializers.py
@@ -1,25 +1,35 @@
 from sqlmodel_serializers import SQLModelSerializer
 
-from events.models import Event, Representation, Offer
+from events.models import Event, Representation, Offer, OfferType
 
 
 class EventLightSerializer(SQLModelSerializer):
+    id: str
+
     class Meta:
         model = Event
         omit = ("organization_id", "organization", "created_at", "updated_at")
 
 
 class RepresentationLightSerializer(SQLModelSerializer):
+    id: str
     event: EventLightSerializer
 
     class Meta:
         model = Representation
-        omit = ("inventories", "participations", "created_at", "updated_at")
+        omit = ("inventories", "participations", "created_at", "updated_at", "event_id")
+
+
+class OfferTypeLightSerializer(SQLModelSerializer):
+    class Meta:
+        model = OfferType
+        fields = ("label",)
 
 
 class OfferLightSerializer(SQLModelSerializer):
-    type_label: str
+    id: str
+    type: OfferTypeLightSerializer
 
     class Meta:
         model = Offer
-        fields = ("name", "description", "type_label")
+        fields = ("id", "name", "type")
