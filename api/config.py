7f06b32c0942723d08cdc0aacc142b84ec6d37fe
liaminec <liaminec@gmail.com>
2025-09-04 02:09:40 +0200

    added .env and changed config to take it into account

--- api/config.py
+++ api/config.py
@@ -1,5 +1,18 @@
+import os
+from dotenv import load_dotenv
+
 from sqlmodel import create_engine
 
+from exceptions import UnsetVarError
+
+load_dotenv()
+
 DEBUG = True
-DB_URL = "sqlite:///data/db/database"
+try:
+    DB_URL = os.environ["DB_URL"]
+except KeyError as kerr:
+    raise UnsetVarError(
+        "You must set the DB_URL env variable (see .env file)"
+    ) from kerr
+
 engine = create_engine(DB_URL, echo=DEBUG)
