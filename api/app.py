cdf5fc7446ee95fb24d08a8396fb98eaa68aa87f
liaminec <liaminec@gmail.com>
2025-09-05 01:32:22 +0200

    routing

--- api/app.py
+++ api/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from participations.routes import router as participations_router
 from users.routes import router as users_router
+from events.routes import router as events_router
 
 app = FastAPI()
 
 app.include_router(participations_router)
 app.include_router(users_router)
+app.include_router(events_router)
